624de019da4ca9535b627f7e33c9e351457a6907
grosser <grosser.michael@gmail.com>
2009-03-13 10:38:32 +0100

    add validation for action

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,4 +1,6 @@
 class Activity < ActiveRecord::Base
   belongs_to :actor, :class_name => 'User'
   belongs_to :subject, :polymorphic=>true
+
+  validates_presence_of :action
 end
--- spec/record_activities_spec.rb
+++ spec/record_activities_spec.rb
@@ -6,43 +6,63 @@ describe :record_activities do
     Comment.delete_all
     User.stamper = User.create!
   end
+
   describe "basics" do
     it "records an activity when a record is created" do
       lambda{Comment.create!(:text=>'x')}.should change(Activity,:count).by(+1)
     end
+
     it "does not record an activity when a record fails to e created" do
       lambda{Comment.create}.should change(Activity,:count).by(0)
     end
   end
+
+  describe 'validations' do
+    subject {Activity.new(:action=>'something',:actor_id=>1)}
+    it {should be_valid}
+
+    it "is not valid without an action" do
+      subject.action=nil
+      should_not be_valid
+    end
+  end
+
   describe "storing" do
     before do
       Comment.create!(:text=>'x')
     end
+
     it "records with current stamper as actor" do
       Activity.first.actor.id.should == User.stamper
     end
+
     it "records the action being performed" do
       Comment.first.save!
       Activity.last.action.should == 'update'
     end
+
     it "records the subject as the item being changed" do
       Activity.first.subject.should == Comment.first
     end
   end
+
   describe "recording custom activities" do
     it "does not store default activities" do
       lambda{Comment2.create!(:text=>'x')}.should_not change(Activity,:count)
     end
+
     it "stores custom activities" do
       c = Comment2.create!(:text=>'x')
       lambda{c.record_activity_foo}.should change(Activity,:count).by(+1)
     end
+
     it "stores custom actions" do
       c = Comment2.create!(:text=>'x')
       c.record_activity_foo
       Activity.first.action.should == 'foo'
     end
   end
+
   describe "destroying a subject" do
     it "removes all activities" do
       x = Comment2.create!(:text=>'x')
